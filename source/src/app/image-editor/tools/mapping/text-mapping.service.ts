3e984e654fca18aed64a6259035f3822b4bd4eb3
vrivasdev <victor.rivas.sistemas@gmail.com>
2020-02-18 17:05:58 -0400

    [feature] Flyer - MLS - Preview - Text mapping

--- source/src/app/image-editor/tools/mapping/text-mapping.service.ts
+++ source/src/app/image-editor/tools/mapping/text-mapping.service.ts
@@ -12,21 +12,26 @@ export class TextMappingService {
             const profile = this.config.get('pixie.profile');
             const mlsVars = ['baths', 'street_number', 'street_name', 'apartment', 'zipcode',
                              'city', 'state', 'country', 'address', 'description', 'type', 'bedrooms', 'price'];
+            let variables;
+            let row;
             let newText = text;
-
             vars.forEach(index => {
-                if (profile[index]) {
-                    if (mlsVars.includes(index) && profile['mls'].length > 0 ) {
-                        /* TODO: if variable is mls */
-                        
-                    } else {
-                        newText = newText.split(`[${index}]`).join(profile[index]);
-                    }
+                if (mlsVars.includes(index) && profile['mls'].length > 0 ) {// MLS: only the first mls
+                    variables = this.mapVariables(profile['mls']); // one or more mls
+                    row = variables[0][index];
+                } else { // Profile: get row
+                    row = profile[index];
                 }
+                newText = newText.split(`[${index}]`).join(row);
             });
-            
             resolve(newText);
         });
+    }
+
+    private mapVariables(data: Array<any> = []): Array<any> {
+        const merged = [];
+        data.forEach(row => merged.push({...row[0], ...row[1]}));
+        return merged;
     }
 
     public filterWords(text: string): Array<string> {
